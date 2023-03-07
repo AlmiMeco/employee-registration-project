fb6d24e53a424710f17b661579a3221040c32d3a
Almi's PC <almimeci@gmail.com>
2023-03-07 17:26:36 -0600

    HTML FILE HARD-CODED ( Data Binded : EmployeeList created & binded )

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,21 +1,40 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
+    private final EmployeeService employeeService;  // [Dependency Injection added]
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
 
     @RequestMapping("/register")
     public String createEmployee(Model model){
 
-
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.stateList());
 
         return "employee/register";
+    }
+
+@PostMapping("/confirm")
+    public String confirmEmployee(@ModelAttribute("employee") Employee employee){
+
+        employeeService.saveEmployee(employee);
+
+        return "employee/confirm";
     }
 
 
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,7 @@ package com.cydeo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -14,7 +15,7 @@ public class Employee {
     private String firstName, lastName, email, password;
 
     private String address, address2, city, state;
-
+@DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     private int zipCode;
